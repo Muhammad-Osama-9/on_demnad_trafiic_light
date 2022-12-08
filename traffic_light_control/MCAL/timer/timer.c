1160db939f840fe5e7588796e13a964fd52da1b7
Muhammad Osama Elaraby <112892754+Muhammad-Osama-9@users.noreply.github.com>
2022-12-08 18:43:36 +0200

    Update timer.c

--- traffic_light_control/MCAL/timer/timer.c
+++ traffic_light_control/MCAL/timer/timer.c
@@ -1,43 +1,48 @@
 /*
  * timer.c
  *
- * Created: 11/11/2022 3:29:12 PM
+ * Created: 12/4/2022 5:32:01 PM
  *  Author: engmu
  */ 
 #include "timer.h"
-volatile uint16 N_s ; 
-volatile uint16 N_ms ; 
+volatile uint16 N ;
+
+volatile float r_num ;
+volatile uint16 init_num ; 
 void d_elay_s (uint16 t_sec)
 {
-	N_s= 0 ;
-	 N_s = (int) t_sec / 0.2621 ; 
-	TCCR0 = 0x00 ; // initi 
-	TCNT0 = 0; 
-	TCCR0 = 0x05; // 1024 prescaller - START Timer
-	while (N_s--)
-	{
-		
-	    	  while (!(GET_BIT(TIFR,0))) ;
-	    	  SET_BIT(TIFR,0) ; // remove FLag 
+	
+	N = (int) ((t_sec / 0.0655 )) ;
+	 r_num = (t_sec / 0.0655 )- N ; 
+	 init_num = (int)(1-r_num)/256 ; 
+	TCCR0 = 0x00 ; // initi
+	TCNT0 = init_num;
+	TCCR0 = 0x04; // 256 prescaller - START Timer
+	while (N--)
+	{		
+		while (!(GET_BIT(TIFR,0))) ;
+		SET_BIT(TIFR,0) ; // remove FLag
 	}
 	//stop the timer
 	TCCR0 = 0x00 ;
-	N_s= 0 ;
+	N= 0 ;
 	TCNT0 = 0x00 ;
 }
 void d_elay_ms (uint16 t_msec)
 {
-  uint16 N = (int)t_msec / 16.384 ; //  (16.384) -->  time to overflow counter with 64 prescaller
-  TCCR0 = 0x00 ; // initi
-  TCNT0 = (int)(t_msec / 0.064 ) / N ;// initial count number 
-  TCCR0 = 0x03; // 64 prescaller - START Timer
-  while (N--)
-  {
-	  
-	  while (!(GET_BIT(TIFR,0))) ; // delay // 
-	  
-	  SET_BIT(TIFR,0) ; // remove FLag
-  }
-  TCNT0 = 0x00 ; 
+	 N = (int)t_msec / 16.384 ; //  (16.384) -->  time to overflow counter with 64 prescaller
+	r_num = (t_msec / 16.384)- N ;
+	init_num = (int)(1-r_num)/256 ;
+	TCCR0 = 0x00 ; // initi
+	TCNT0 = init_num ;// initial count number
+	TCCR0 = 0x03; // 64 prescaller - START Timer
+	while (N--)
+	{
+		
+		while (!(GET_BIT(TIFR,0))) ; // delay //
+		
+		SET_BIT(TIFR,0) ; // remove FLag
+	}
+	TCNT0 = 0x00 ;
 	
 }
